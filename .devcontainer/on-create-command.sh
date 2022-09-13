afeb2a583450c7d0fec1c38f0032bfbf0160b48d
Preston Martin <pmartindev@github.com>
2022-09-13 16:20:25 -0500

    Added on create command to move some repos.

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,1 +1,6 @@
-echo "on-create"
+#!/bin/sh
+cd /source
+cp -r a-bad-monorepo-clean a-bad-monorepo-filter-repo
+cp -r a-bad-monorepo-clean a-bad-monorepo-lfs
+cp -r a-bad-monorepo-clean a-bad-monorepo-graft
+mv a-bad-monorepo-clean a-bad-monorepo
