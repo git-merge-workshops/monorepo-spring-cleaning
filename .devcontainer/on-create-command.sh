3788b103bdc1604611fbd24b95e80ab1e6a510a0
Preston Martin <pmartindev@github.com>
2022-09-14 12:08:46 -0500

    Update on-create-command.sh
    Update devcontainer create script

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 cd /source
 cp -r a-bad-monorepo-clean a-bad-monorepo-filter-repo
-cp -r a-bad-monorepo-clean a-bad-monorepo-lfs
-cp -r a-bad-monorepo-clean a-bad-monorepo-graft
 mv a-bad-monorepo-clean a-bad-monorepo
